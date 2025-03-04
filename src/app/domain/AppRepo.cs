bc6faed89e2cfef18d06126634e6c18cb509328e
良彰 八木 <gurenlagan0411@yahoo.co.jp>
2025-03-04 22:35:12 +0900

    AppとSplash処理実装中

--- src/app/domain/AppRepo.cs
+++ src/app/domain/AppRepo.cs
@@ -5,12 +5,23 @@ using System;
 public interface IAppRepo : IDisposable
 {
     event Action? GameEntered;
+
+    event Action? SplashScreenSkipped;
+
+    void SkipSplashScreen();
 }
 
 
 public class AppRepo : IAppRepo
 {
     public event Action? GameEntered;
+
+    public event Action? SplashScreenSkipped;
+
+    public void SkipSplashScreen()
+    {
+        SplashScreenSkipped?.Invoke();
+    }
 
     public void Dispose() { }
 }
---
+++ src/splash/Splash.cs
@@ -0,0 +1,43 @@
+namespace EternalJourney;
+
+using Chickensoft.AutoInject;
+using Chickensoft.GodotNodeInterfaces;
+using Chickensoft.Introspection;
+using Godot;
+
+public interface ISplash : IControl;
+
+[Meta(typeof(IAutoNode))]
+public partial class Splash : Control, ISplash
+{
+    public override void _Notification(int what) => this.Notify(what);
+
+    [Node]
+    public ITimer SplashTimer { get; set; } = default!;
+
+    [Dependency]
+    public IAppRepo AppRepo => this.DependOn<IAppRepo>();
+
+    public void OnReady()
+    {
+        SplashTimer.Timeout += SplashTimeOut;
+        SplashTimer.SetWaitTime(2);
+        SplashTimer.Start();
+    }
+
+    public void SplashTimeOut()
+    {
+        GD.Print("splash timeout");
+        AppRepo.SkipSplashScreen();
+    }
+
+    // // Called when the node enters the scene tree for the first time.
+    // public override void _Ready()
+    // {
+    // }
+
+    // // Called every frame. 'delta' is the elapsed time since the previous frame.
+    // public override void _Process(double delta)
+    // {
+    // }
+}
