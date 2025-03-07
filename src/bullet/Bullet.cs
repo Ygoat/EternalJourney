5d9f41ecac8063d3e5035ea62ff3592291e977c9
良彰 八木 <gurenlagan0411@yahoo.co.jp>
2025-03-08 04:11:42 +0900

    弾丸ロジック

--- src/bullet/Bullet.cs
+++ src/bullet/Bullet.cs
@@ -5,18 +5,29 @@ using Chickensoft.GodotNodeInterfaces;
 using Chickensoft.Introspection;
 using Godot;
 
-public interface IBullet : INode2D { }
+public interface IBullet : INode2D
+{
+    public event Bullet.HitEventHandler Hit;
+}
 
 [Meta(typeof(IAutoNode))]
 public partial class Bullet : Node2D, IBullet
 {
     public override void _Notification(int what) => this.Notify(what);
 
+    [Signal]
+    public delegate void HitEventHandler();
+
     [Node]
     public Area2D Area2D { get; set; } = default!;
 
     public void OnReady()
     {
+        Area2D.AreaEntered += OnAreaEntered;
+    }
 
+    public void OnAreaEntered(Area2D area)
+    {
+        EmitSignal(SignalName.Hit);
     }
 }
