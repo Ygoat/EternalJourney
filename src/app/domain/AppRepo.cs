679ab19a44376a8bf5c2b8eece103d32e4ce2d4a
Yoshiaki Yagi <yoshiaki.yagi@sola-air.com>
2025-03-07 23:20:20 +0900

    コメント追加

--- src/app/domain/AppRepo.cs
+++ src/app/domain/AppRepo.cs
@@ -2,40 +2,87 @@ namespace EternalJourney.App.Domain;
 
 using System;
 
+/// <summary>
+/// アプリケーションレポジトリインターフェース
+/// </summary>
 public interface IAppRepo : IDisposable
 {
+    /// <summary>
+    /// ゲーム開始イベント
+    /// </summary>
     event Action? GameEntered;
 
+    /// <summary>
+    /// スプラッシュ画面スキップイベント
+    /// </summary>
     event Action? SplashScreenSkipped;
+
+    /// <summary>
+    /// メインメニュー開始イベント
+    /// </summary>
     event Action? MainMenuEntered;
+
+    /// <summary>
+    /// スプラッシュ画面スキップイベントメソッド
+    /// </summary>
 
     void SkipSplashScreen();
 
+    /// <summary>
+    /// ゲーム開始イベントメソッド
+    /// </summary>
     void OnEnterGame();
+
+    /// <summary>
+    /// メインメニュー開始イベントメソッド
+    /// </summary>
     void OnMainMenuEntered();
 }
 
 
 public class AppRepo : IAppRepo
 {
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     public event Action? GameEntered;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     public event Action? SplashScreenSkipped;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     public event Action? MainMenuEntered;
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     public void SkipSplashScreen()
     {
         SplashScreenSkipped?.Invoke();
     }
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     public void OnMainMenuEntered()
     {
         MainMenuEntered?.Invoke();
     }
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     public void OnEnterGame()
     {
         GameEntered?.Invoke();
     }
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
     public void Dispose() { }
 }
