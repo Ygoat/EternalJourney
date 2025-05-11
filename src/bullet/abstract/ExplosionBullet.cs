776ca2735226fa9cbc3cfa4667fbdc043847cbc6
良彰 八木 <gurenlagan0411@yahoo.co.jp>
2025-05-11 18:17:50 +0900

    ノード追加までのサンプルスクリプト作成

---
+++ src/bullet/abstract/ExplosionBullet.cs
@@ -0,0 +1,56 @@
+namespace EternalJourney.Bullet.Abstract;
+
+using Chickensoft.AutoInject;
+using Chickensoft.GodotNodeInterfaces;
+using Chickensoft.Introspection;
+using EternalJourney.Bullet.Abstract.Base;
+using Godot;
+
+
+public interface IExplosionBullet : IBaseBullet
+{
+}
+
+[Meta(typeof(IAutoNode))]
+public partial class ExplosionBullet : BaseBullet, IExplosionBullet
+{
+    public override void _Notification(int what) => this.Notify(what);
+
+    [Node]
+    public ITimer BlastTimer { get; set; } = default!;
+
+    [Node]
+    public ICollisionShape2D BulletCollisionShape2D { get; set; } = default!;
+
+    [Node]
+    public IArea2D Blast { get; set; } = default!;
+
+    [Node]
+    public ICollisionShape2D BlastCollisionShape2D { get; set; } = default!;
+
+    public void Setup()
+    {
+
+    }
+
+    public void OnResolved()
+    {
+        BlastTimer.Timeout += OnBlastTimerTimeout;
+        BlastTimer.SetWaitTime(5);
+        BlastTimer.OneShot = true;
+        BlastTimer.Start();
+
+        SetPhysicsProcess(true);
+    }
+
+    public void OnBlastTimerTimeout()
+    {
+        Blast.Show();
+        SetPhysicsProcess(false);
+    }
+
+    public void OnPhysicsProcess(double delta)
+    {
+        GlobalPosition += new Vector2(1, 0);
+    }
+}
