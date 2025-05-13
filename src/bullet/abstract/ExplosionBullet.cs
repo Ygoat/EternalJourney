fdfad5906d393e69220917289286b67b4a8c8838
良彰 八木 <gurenlagan0411@yahoo.co.jp>
2025-05-14 00:46:53 +0900

    サンプルスクリプト作成完了

--- src/bullet/abstract/ExplosionBullet.cs
+++ src/bullet/abstract/ExplosionBullet.cs
@@ -1,11 +1,17 @@
 namespace EternalJourney.Bullet.Abstract;
 
+using System;
+using System.Net.Http.Headers;
+using System.Xml;
 using Chickensoft.AutoInject;
 using Chickensoft.GodotNodeInterfaces;
 using Chickensoft.Introspection;
 using EternalJourney.Bullet.Abstract.Base;
+using EternalJourney.Bullet.Abstract.State;
+using EternalJourney.Common.Traits;
+using EternalJourney.Cores.Consts;
+using EternalJourney.Enemy.Abstract.Base;
 using Godot;
-
 
 public interface IExplosionBullet : IBaseBullet
 {
@@ -15,6 +21,10 @@ public interface IExplosionBullet : IBaseBullet
 public partial class ExplosionBullet : BaseBullet, IExplosionBullet
 {
     public override void _Notification(int what) => this.Notify(what);
+
+    public ExplosionBulletLogic ExplosionBulletLogic { get; set; } = default!;
+
+    public ExplosionBulletLogic.IBinding ExplosionBulletBinding { get; set; } = default!;
 
     [Node]
     public ITimer BlastTimer { get; set; } = default!;
@@ -26,36 +36,163 @@ public partial class ExplosionBullet : BaseBullet, IExplosionBullet
     public IColorRect BulletColorRect { get; set; } = default!;
 
     [Node]
+    public ICollisionShape2D BlastCollisionShape2D { get; set; } = default!;
+
+    [Node]
     public IColorRect BlastColorRect { get; set; } = default!;
 
     [Node]
-    public ICollisionShape2D BlastCollisionShape2D { get; set; } = default!;
+    public IVisibleOnScreenNotifier2D VisibleOnScreenNotifier2D { get; set; } = default!;
+
+    public Vector2 Direction { get; set; } = default!;
 
     public override void Setup()
     {
         base.Setup();
+
+        // ステートロジックのインスタンス化
+        ExplosionBulletLogic = new ExplosionBulletLogic();
+        ExplosionBulletBinding = ExplosionBulletLogic.Bind();
+        ExplosionBulletLogic.Set(this as IBaseBullet);
+
+        // コリジョンレイヤーの設定(自身の衝突レイヤーをBullet)
+        CollisionLayer = CollisionEntity.Bullet;
+        // コリジョンマスクの設定(衝突対象とする衝突レイヤーをEnemy)
+        CollisionMask = CollisionEntity.Enemy;
+
+        Status = new Status { Spd = 1, MaxDur = 0.1f, CurrentDur = 0.1f };
     }
 
     public override void OnResolved()
     {
         base.OnResolved();
 
-        BlastTimer.Timeout += OnBlastTimerTimeout;
-        BlastTimer.SetWaitTime(5);
-        BlastTimer.OneShot = true;
-        BlastTimer.Start();
+        ExplosionBulletBinding
+            .When<ExplosionBulletLogic.State.EmitWait>(state =>
+            {
+                // 爆風テクスチャ非表示と爆風当たり判定無効
+                BlastColorRect.Hide();
+                BlastCollisionShape2D.Disabled = true;
+            })
+            .Handle((in ExplosionBulletLogic.Output.Emitted output) =>
+            {
+                // 射出時の位置を設定(武器の発射口の位置)
+                GlobalPosition = output.ShotGlobalPosition;
+                // 射出時の方向を設定(武器の向いている方向)
+                Direction = new Vector2(1, 0).Rotated(output.ShotGlobalAngle);
+                // 弾丸の向きを設定（武器の向いている方向）
+                Rotation = output.ShotGlobalAngle;
+            })
+            .When<ExplosionBulletLogic.State.InFlight>(state =>
+            {
+                // 弾丸テクスチャ表示と弾丸当たり判定有効化
+                BulletColorRect.Show();
+                BulletCollisionShape2D.Disabled = false;
 
-        SetPhysicsProcess(true);
+                SetPhysicsProcess(true);
+            })
+            .Handle((in ExplosionBulletLogic.Output.Move output) =>
+            {
+                GlobalPosition += output.NextPositionDelta;
+            })
+            .Handle((in ExplosionBulletLogic.Output.CurrentDurChange output) =>
+            {
+                Status.CurrentDur = output.CurrentDur;
+            })
+            .Handle((in ExplosionBulletLogic.Output.Collapse output) =>
+            {
+                SetPhysicsProcess(false);
+            })
+            .When<ExplosionBulletLogic.State.Blast>(state =>
+            {
+                // 弾丸テクスチャ非表示と弾丸当たり判定無効化
+                BulletColorRect.Hide();
+                BulletCollisionShape2D.Disabled = true;
+
+                // 爆風テクスチャ表示と爆風当たり判定有効化
+                BlastColorRect.Show();
+                BlastCollisionShape2D.Disabled = false;
+            })
+            .Handle((in ExplosionBulletLogic.Output.BlastEnd _) =>
+            {
+                // 爆風テクスチャ表示と爆風当たり判定有効化
+                BlastColorRect.Show();
+                BlastCollisionShape2D.Disabled = false;
+
+                // フレーム終わりにRemoveSelf()呼び出し
+                CallDeferred(nameof(RemoveSelf));
+            });
+
+        // コリジョンイベント設定
+        AreaEntered += OnAreaEntered;
+        // 画面外イベント
+        VisibleOnScreenNotifier2D.ScreenExited += OnScreenExited;
+        // ロジック初期化
+        ExplosionBulletLogic.Start();
+        // Emit(new Vector2(1, 0), 0);
+        // ExplosionBulletLogic.Input(new ExplosionBulletLogic.Input.EnemyHit());
+        // ExplosionBulletLogic.Input(new ExplosionBulletLogic.Input.BlastTimerTimeout());
+        // InitializeBullet();
+        // Emit(new Vector2(1, 0), 0);
+        // トップレベルオブジェクトとして扱う（親ノードのRotationの影響を受けないようにするため）
+        TopLevel = true;
     }
 
     public void OnPhysicsProcess(double delta)
     {
-        GlobalPosition += new Vector2(1, 0);
+        ExplosionBulletLogic.Input(new ExplosionBulletLogic.Input.PhysicsProcess(new Vector2(1, 0), Status.Spd));
     }
 
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="shotGlobalPosition"></param>
+    /// <param name="shotGlobalAngle"></param>
+    public override void Emit(Vector2 shotGlobalPosition, float shotGlobalAngle)
+    {
+        ExplosionBulletLogic.Input(new ExplosionBulletLogic.Input.Emit(shotGlobalPosition, shotGlobalAngle));
+    }
+
+
+
+    /// <summary>
+    /// 弾丸/爆風コリジョンエリア進入イベントファンクション
+    /// </summary>
+    /// <param name="area"></param>
+    private void OnAreaEntered(Area2D area)
+    {
+        if (area is IBaseEnemy enemy)
+        {
+            ExplosionBulletLogic.Input(new ExplosionBulletLogic.Input.EnemyHit(enemy));
+        }
+    }
+
+    /// <summary>
+    /// スクリーン外イベントファンクション
+    /// </summary>
+    private void OnScreenExited()
+    {
+        ExplosionBulletLogic.Input(new ExplosionBulletLogic.Input.Miss());
+    }
+
+    /// <summary>
+    /// 爆風タイマータイムアウトイベントファンクション
+    /// </summary>
     private void OnBlastTimerTimeout()
     {
-        BlastColorRect.Show();
-        SetPhysicsProcess(false);
+        ExplosionBulletLogic.Input(new ExplosionBulletLogic.Input.BlastTimerTimeout());
+    }
+
+    /// <summary>
+    /// 弾丸初期化
+    /// </summary>
+    public void InitializeBullet()
+    {
+        // グローバル座標の初期化
+        GlobalPosition = new Vector2(0, 0);
+        // 方向を初期化
+        Direction = new Vector2(0, 0);
+        // 耐久値を回復
+        Status.CurrentDur = Status.MaxDur;
     }
 }
---
+++ src/bullet/abstract/state/ExplosionBulletLogic.cs
@@ -0,0 +1,198 @@
+namespace EternalJourney.Bullet.Abstract.State;
+
+using Chickensoft.Introspection;
+using Chickensoft.LogicBlocks;
+using EternalJourney.Battle.Domain;
+using EternalJourney.Bullet.Abstract.Base;
+using EternalJourney.Enemy.Abstract.Base;
+using Godot;
+
+
+
+
+/// <summary>
+/// 弾丸ロジックインターフェース
+/// </summary>
+public interface IExplosionBulletLogic : ILogicBlock<ExplosionBulletLogic.State>;
+
+/// <summary>
+/// 弾丸ロジック
+/// </summary>
+[Meta, LogicBlock(typeof(State), Diagram = true)]
+public partial class ExplosionBulletLogic : LogicBlock<ExplosionBulletLogic.State>, IExplosionBulletLogic
+{
+    /// <summary>
+    /// 初期状態
+    /// </summary>
+    /// <returns></returns>
+    public override Transition GetInitialState() => To<State.EmitWait>();
+
+    /// <summary>
+    /// 入力定義
+    /// </summary>
+    public static class Input
+    {
+        /// <summary>
+        /// 射撃
+        /// </summary>
+        public readonly record struct Emit(Vector2 ShotGlobalPosition, float ShotGlobalAngle);
+
+        /// <summary>
+        /// ヒット
+        /// </summary>
+        public readonly record struct EnemyHit(IBaseEnemy BaseEnemy);
+
+        /// <summary>
+        /// ミス
+        /// </summary>
+        public readonly record struct Miss;
+
+        /// <summary>
+        /// 物理処理
+        /// </summary>
+        /// <param name="Direction"></param>
+        /// <param name="Speed"></param>
+        public readonly record struct PhysicsProcess(Vector2 Direction, float Speed);
+
+        /// <summary>
+        /// 爆風タイムアウト
+        /// </summary>
+        public readonly record struct BlastTimerTimeout();
+    }
+
+    /// <summary>
+    /// 出力定義
+    /// </summary>
+    public static class Output
+    {
+        /// <summary>
+        /// 射出後
+        /// </summary>
+        public readonly record struct Emitted(Vector2 ShotGlobalPosition, float ShotGlobalAngle);
+
+        /// <summary>
+        /// 耐久値変化
+        /// </summary>
+        public readonly record struct CurrentDurChange(float CurrentDur);
+
+        /// <summary>
+        /// 崩壊
+        /// </summary>
+        public readonly record struct Collapse;
+
+        /// <summary>
+        /// 移動
+        /// </summary>
+        public readonly record struct Move(Vector2 NextPositionDelta);
+
+        /// <summary>
+        /// 爆風終了
+        /// </summary>
+        public readonly record struct BlastEnd();
+    }
+
+    /// <summary>
+    /// 状態定義
+    /// </summary>
+    public abstract record State : StateLogic<State>
+    {
+        /// <summary>
+        /// ロード
+        /// </summary>
+        public record EmitWait : State, IGet<Input.Emit>
+        {
+            public EmitWait()
+            {
+            }
+
+            public Transition On(in Input.Emit input)
+            {
+                Output(new Output.Emitted(input.ShotGlobalPosition, input.ShotGlobalAngle));
+                return To<InFlight>();
+            }
+        }
+
+        /// <summary>
+        /// 飛翔
+        /// </summary>
+        public record InFlight : State, IGet<Input.PhysicsProcess>, IGet<Input.EnemyHit>, IGet<Input.Miss>
+        {
+            public InFlight()
+            {
+            }
+
+            public Transition On(in Input.PhysicsProcess input)
+            {
+                // 位置を更新
+                Vector2 nextPositionDelta = input.Direction.Normalized() * input.Speed;
+                Output(new Output.Move(nextPositionDelta));
+                // 耐久値チェック
+                IBaseBullet baseBullet = Get<IBaseBullet>();
+                return CheckUnderZeroDurability(baseBullet.Status.CurrentDur);
+            }
+
+            public Transition On(in Input.EnemyHit input)
+            {
+                GD.Print("hit");
+                // 耐久値減少
+                // IBattleRepo battleRepo = Get<IBattleRepo>();
+                IBaseBullet baseBullet = Get<IBaseBullet>();
+                // float currentDur = battleRepo.ReduceBulletDurability(baseBullet.Status.CurrentDur, 1.0f);
+                float currentDur = baseBullet.Status.CurrentDur - 1.0f;
+                GD.Print(currentDur);
+                // baseBullet.StatusEffectServerManager.Apply(input.BaseEnemy.StatusEffectReceiverManager);
+                // 耐久値変更を通知
+                Output(new Output.CurrentDurChange(currentDur));
+                // 耐久値チェック
+                return CheckUnderZeroDurability(currentDur);
+            }
+
+            public Transition On(in Input.Miss input)
+            {
+                // 崩壊を出力する
+                Output(new Output.Collapse());
+                // 射出待機に遷移する
+                return To<EmitWait>();
+            }
+
+            private Transition CheckUnderZeroDurability(float currentDur)
+            {
+                // 耐久値が0以下の場合
+                if (currentDur <= 0)
+                {
+                    GD.Print("under");
+                    // 崩壊を出力する
+                    Output(new Output.Collapse());
+                    // 射出待機に遷移する
+                    return To<Blast>();
+                }
+                GD.Print("upper");
+                return ToSelf();
+            }
+        }
+
+        /// <summary>
+        /// 爆風
+        /// </summary>
+        public record Blast : State, IGet<Input.BlastTimerTimeout>, IGet<Input.EnemyHit>
+        {
+            public Blast()
+            {
+            }
+
+            public Transition On(in Input.BlastTimerTimeout input)
+            {
+                // 爆風終了を出力
+                Output(new Output.BlastEnd());
+                // 射出待機に遷移
+                return To<EmitWait>();
+            }
+
+            public Transition On(in Input.EnemyHit input)
+            {
+                GD.Print("BlastHit");
+                return ToSelf();
+            }
+        }
+    }
+}
